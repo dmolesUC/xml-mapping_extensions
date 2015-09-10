26baaa91493b9cd3803f345130785e97528b693d
David Moles <david.moles@ucop.edu>
2015-09-10 14:28:18 -0700

    Fix modules

--- lib/xml/mapping_extensions.rb
+++ lib/xml/mapping_extensions.rb
@@ -1,5 +1,7 @@
-# Additional mapping nodes and other utility classes for working with
-# [XML::Mapping](http://multi-io.github.io/xml-mapping/)
-module Resync
-  Dir.glob(File.expand_path('../mapping_extensions/*.rb', __FILE__), &method(:require))
+module XML
+  # Additional mapping nodes and other utility classes for working with
+  # [XML::Mapping](http://multi-io.github.io/xml-mapping/)
+  module MappingExtensions
+    Dir.glob(File.expand_path('../mapping_extensions/*.rb', __FILE__), &method(:require))
+  end
 end
