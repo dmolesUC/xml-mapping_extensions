831f56b9150c33651f57e010c204be85166e5b5e
David Moles <david.moles@ucop.edu>
2016-05-16 16:43:11 -0700

    Added (failing) multiple-namespace test

--- spec/unit/xml/mapping_extensions/namespaced_spec.rb
+++ spec/unit/xml/mapping_extensions/namespaced_spec.rb
@@ -48,6 +48,51 @@ module XML
         it 'parses XML with a default namespace'
         it 'parses XML with a prefixed namespace'
       end
+
+      describe 'namespace' do
+        it 'takes a string' do
+          class StringNS
+            include Namespaced
+
+            namespace 'http://example.org/string-ns'
+          end
+
+          expect(StringNS.namespace).to eq(Namespace.new(uri: 'http://example.org/string-ns'))
+        end
+
+        it 'takes an array' do
+          class MultipleNS
+            include Namespaced
+
+            namespace([
+              Namespace.new(uri: 'http://example.org/ns1'),
+              Namespace.new(uri: 'http://example.org/ns2', prefix: 'ns2'),
+              Namespace.new(uri: 'http://example.org/ns3', prefix: 'ns3', schema_location: 'http://example.org/ns3.xsd'),
+              Namespace.new(uri: 'http://example.org/ns4', prefix: 'ns4', schema_location: 'http://example.org/ns4.xsd')
+            ])
+
+            root_element_name 'multi'
+            text_node :attr, '@attr'
+            text_node :text, 'text()'
+          end
+
+          obj = MultipleNS.new
+          obj.attr = 'attr value'
+          obj.text = 'text value'
+
+          expected = '<multi
+            xmlns="http://example.org/ns1"
+            xmlns:ns2="http://example.org/ns2"
+            xmlns:ns3="http://example.org/ns3"
+            xmlns:ns4="http://example.org/ns4"
+            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xsi:schemaLocation="http://example.org/ns3 http://example.org/ns3.xsd http://example.org/ns4 http://example.org/ns4.xsd"
+          </multi>'
+
+          expect(obj.write_xml).to be_xml(expected)
+        end
+      end
+
     end
   end
 end
